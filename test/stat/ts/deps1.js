6e869c3377e053989bca90e0fbcb1ba3e891b70d
brn <dobaw20@gmail.com>
2014-10-27 13:00:45 +0900

    #1 fix typescript@1.1.0

---
+++ test/stat/ts/deps1.js
@@ -0,0 +1,6 @@
+var Deps1 = (function () {
+    function Deps1() {
+    }
+    return Deps1;
+})();
+module.exports = Deps1;
---
+++ test/stat/ts/deps2.js
@@ -0,0 +1,6 @@
+var Deps2 = (function () {
+    function Deps2() {
+    }
+    return Deps2;
+})();
+module.exports = Deps2;
