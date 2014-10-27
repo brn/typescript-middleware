59393724353411abe0f281ebe14bb13394657b76
brn <dobaw20@gmail.com>
2014-10-27 13:02:15 +0900

    remove junk files.

--- test/stat/ts/deps1.js
+++
@@ -1,6 +0,0 @@
-var Deps1 = (function () {
-    function Deps1() {
-    }
-    return Deps1;
-})();
-module.exports = Deps1;
--- test/stat/ts/deps2.js
+++
@@ -1,6 +0,0 @@
-var Deps2 = (function () {
-    function Deps2() {
-    }
-    return Deps2;
-})();
-module.exports = Deps2;
